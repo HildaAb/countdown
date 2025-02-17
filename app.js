131e927365df15f4329e570e9acb56f9c927389d
HildaAb <ilona.aase@gmail.com>
2025-02-18 00:41:55 +0100

    fixed the time för countdown in js

---
+++ app.js
@@ -0,0 +1,26 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+function updateCountdown() {
+  const now = new Date();
+  const nextYear = now.getFullYear() + 1;
+  const newYear = new Date(`January 1, ${nextYear} 00:00:00`);
+  const timeDifference = newYear - now;
+
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  console.log(days, hours, minutes, seconds);
+}
+
+// Uppdatera nedräkningen varje sekund
+setInterval(updateCountdown, 1000);
+
+// Initial uppdatering
+updateCountdown();
